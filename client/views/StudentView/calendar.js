fdf9f81741c3d1a845497d47bffa0a3f5dde66a0
Matt Greenwolfe <solargreen@mindspring.com>
2014-06-01 12:52:57 -0400

    saving current state to try RowbyRowcalendar branch

--- client/views/StudentView/calendar.js
+++ client/views/StudentView/calendar.js
@@ -3,9 +3,9 @@ Template.calendar.helpers({
     var startDate = Session.get('calStartDate');
     var endDate = Session.get('calEndDate');
     var calendarWeeks, weeks; 
-    startDate = moment(startDate,'ddd[,] MMM D YYYY').day("Monday");
+    startDate = moment(startDate,'ddd[,] MMM D YYYY'); //.day("Monday");
     calendarWeeks = [{monOfWeek : startDate.format('ddd[,] MMM D YYYY')}]
-    endDate = moment(endDate,'ddd[,] MMM D YYYY').day("Monday");
+    endDate = moment(endDate,'ddd[,] MMM D YYYY'); //.day("Monday");
     weeks = endDate.diff(startDate,'weeks');
     for (week = 1; week <= weeks; week++) {
       calendarWeeks.push({monOfWeek : startDate.add("weeks",1).format('ddd[,] MMM D YYYY')}); 
