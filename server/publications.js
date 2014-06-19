469d58418e264aaa35e07dbaf19e9af4d6d6d30e
Matt Greenwolfe <solargreen@mindspring.com>
2014-06-19 07:31:56 -0400

    stuck trying to handle fixtures for links ... maybe easier just to create the submit form?

--- server/publications.js
+++ server/publications.js
@@ -2,10 +2,14 @@ Meteor.publish('activities',function() {
     return Activities.find();
 });
 Meteor.publish('calendarEvents',function(currentUserID) {
-  return CalendarEvents.find({$or: [ {group: {$in: [currentUserID]}},{invite: {$in: [currentUserID]}} ]});  
+  return CalendarEvents.find({$or: [ {group: {$in: [currentUserID,'_ALL_']}},{invite: {$in: [currentUserID,'_ALL_']}} ]});  
 });
 Meteor.publish('userList',function() {
   return Meteor.users.find({},{fields : {username : 1}});
 });
+Meteor.publish('links',function(currentUserID) {
+  return Links.find( {group: {$in: [currentUserID,'_ALL_']} });
+});
+//may want to publish all and move the selectors to the subscriptions, so that the teacher can see everyone's collection items
 
 
