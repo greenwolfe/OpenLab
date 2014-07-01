9c2bfbd958406939504f930764453065f3dc2c42
Matt Greenwolfe <solargreen@mindspring.com>
2014-07-01 05:46:33 -0400

    student and teacher roles working with some validation in the server-side method.

--- server/users.js
+++ server/users.js
@@ -16,18 +16,13 @@ if (Meteor.isServer) {
 
 Meteor.methods({
   enrollStudent: function(userID) {
-    console.log('on server in enrollStudent');
-    Roles.addUsersToRoles(userID,['student']);
-/*    var currentUserId = Meteor.userId();
+    var currentUserId = Meteor.userId();
     if ( !(currentUserId && (userID == currentUserId)) ) {
-      console.log('    throwing error');
       throw new Meteor.Error(401, "Cannot Enroll as Student");
     };
-    console.log('   past error');
     if (!Roles.userIsInRole(userID,['teacher','student'])) {
-      console.log('    adding student role');
       Roles.addUsersToRoles(userID,['student']);
-    }; */
+    }; 
   }
 });
 
