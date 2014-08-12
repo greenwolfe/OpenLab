c5d60e11a9392caf8058ef618d74439bd452c4bc
Matt Greenwolfe <solargreen@mindspring.com>
2014-08-12 07:48:06 -0400

    working on standards page ... need route and link

---
+++ client/views/standard/standard_page.js
@@ -0,0 +1,49 @@
+    /************************************/
+   /*** Template.standardDescription ***/
+  /************************************/
+
+var defaultText = 'Provide an explanatino for this standard.';
+
+Template.standardDescription.helpers({
+  description:  function() { 
+    var desc = Standards.findOne(this._id).description;
+    if (Roles.userIsInRole(Meteor.userId(),'teacher')) {
+      return (_.clean(_.stripTags(desc))) ? desc : defaultText;
+    } else {
+      return (desc) ? desc : '';
+    }
+  },
+  defaultTextActive: function() {
+    var desc = Standards.findOne(this._id).description;
+    return (_.clean(_.stripTags(desc))) ? '' : 'defaultTextActive';
+  }
+});
+
+Template.standardDescription.events({
+  'click .editDescription': function(event,tmpl) {
+    var $descriptionText = $('#descriptionText');   
+    var $updateButton = $(event.target).parent().parent().find('.updateDescriptionContainer');
+    $descriptionText.addClass('editing').addClass('defaultTextActive');
+    $descriptionText.hallo(hallosettings(true)).bind( "hallodeactivated", function(event) { //hallomodified
+      var standardID = $(event.target).data('standardid');
+      var currentText = Standards.findOne(standardID).description;
+      currentText = (_.clean(_.stripTags(currentText))) ? currentText: defaultText;
+      $descriptionText.removeClass('editing').addClass('defaultTextActive');
+      $descriptionText.hallo({editable: false});
+      $descriptionText.html(currentText);
+      $updateButton.addClass('hidden');
+    });
+    $descriptionText.focus();
+    $updateButton.removeClass('hidden');
+  },
+  'mousedown #updateDescription': function(event,tmpl) {
+    //can't use click because have to catch this before the hallodeactivated binding
+    var nS = {
+      _id: $(event.target).data('standardid'),
+      description: $(event.target).parent().parent().find('#descriptionText').html()
+    };
+    Meteor.call('updateStandard', nS,
+      function(error, id) {if (error) return alert(error.reason);}
+    );
+  }
+}); 
