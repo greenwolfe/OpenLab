6029702dc5da3e10d224fc349a8ac7375774d008
Matt Greenwolfe <solargreen@mindspring.com>
2014-09-11 06:10:11 -0400

    counters coming soon - alert and error/warning

--- client/views/student/studentView.js
+++ client/views/student/studentView.js
@@ -1,4 +1,12 @@
-
+Template.studentView.helpers({
+  counters: function() {  //add dismiss button and record state in user profile
+                      //put help button in main toolbar to reveal again
+    return "Dr. Greenwolfe's students have made ___ comments on wiki pages. You have completed ___ of them."
+  },
+  reminder: function() {
+    return "Counters for wiki pages, high-quality comments, and PGA coming soon and will then be parts of your first PGA grade. <br/> <ul> <li>Make two high-quality comments on position graph wikis. </li>  <li> Claim and make a wiki page for velocity graphs. </li> <li> Make sure your wiki pages have a PGA block. </li> <li> Leave a personal PGA comment for one velocity activity and one position activity in openlab. </li></ul> "
+  }
+});
 //Template.studentView.rendered = function () {
   //$('#appointments .Model p').draggable( DragOpt('.daysActivities') );
   //$('#ListOfLists').tabs(); 
